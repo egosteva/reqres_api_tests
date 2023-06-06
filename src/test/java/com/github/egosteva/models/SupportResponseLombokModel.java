d4c8e7ff02ee6ec9d495d022875439d48671aeea
egosteva <elenagosteva.qa@gmail.com>
2023-06-06 18:18:05 +0300

    init commit

---
+++ src/test/java/com/github/egosteva/models/SupportResponseLombokModel.java
@@ -0,0 +1,8 @@
+package com.github.egosteva.models;
+
+import lombok.Data;
+
+@Data
+public class SupportResponseLombokModel {
+    public String url, text;
+}
---
+++ src/test/java/com/github/egosteva/models/UnsuccessfulRegisterBodyLombokModel.java
@@ -0,0 +1,8 @@
+package com.github.egosteva.models;
+
+import lombok.Data;
+
+@Data
+public class UnsuccessfulRegisterBodyLombokModel {
+    String email;
+}
